ba6280bbf4e0e4c1dafbeec647d0d2c2388b3cee
Fer Barraza <reikafer@gmail.com>
2019-08-12 11:53:46 -0700

    Librerias agregadas

--- EjercicioLibrerias/Program.cs
+++ EjercicioLibrerias/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 
 namespace EjercicioLibrerias
 {
